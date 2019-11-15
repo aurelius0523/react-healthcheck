c539cf41eead8adc0579fb529e0cf4e99399a3ba
Kim Loong Tan <aurelius0523>
2019-11-16 00:01:59 +0800

    Update readme, Remove unnecessary codes

--- src/components/views/ApiCard/ApiCard.tsx
+++ src/components/views/ApiCard/ApiCard.tsx
@@ -1,47 +1,17 @@
-import React, { useRef } from "react";
+import { Button } from "@material-ui/core";
+import React from "react";
 import { Card } from "../../common/card/Card";
 import styles from "./ApiCard.module.scss";
 import ApiCardlet from "./ApiCardlet/ApiCardlet";
-import ApiCardPresenter from "./ApiCardPresenter";
-import { Button } from "@material-ui/core";
-export interface ApiCardFunction {
-
-}
-
-const apiList = [
-    {
-        name: "JSON placeholder healthcheck",
-        url: "https://jsonplaceholder.typicode.com/posts",
-        method: "POST",
-        username: "",
-        password: "",
-        requestBody: "{\"wow\": \"1\"}"
-    },
-    {
-        name: "JSON placeholder healthcheck 2",
-        url: "https://jsonplaceholder.typicode.com/posts/1",
-        method: "GET",
-        username: "",
-        password: ""
-    }
-]
+import apiList from "../../../resources/api-list.json";
 
 const ApiCard: React.FC = () => {
-    const componentFunction = () => {
-
-    }
-
-    let presenter = useRef(new ApiCardPresenter(componentFunction));
-
     return (
         <div className={`${styles.api_card_container}`}>
             <Card title={"Healthcheck"} subtitle={"check if apis are working fine"}>
-                <div className={`${styles.add_section}`}>
-                    <Button variant="outlined" color="inherit" size="small">Add new healthcheck</Button>
-                </div>
                 <div className={`${styles.cardlet_section} ${styles.test}`}>
                     {apiList && apiList.map((api) => {
-                        return <ApiCardlet {...api} />
+                        return <ApiCardlet {...api} key={api.url + api.method} />
                     })}
                 </div>
             </Card>
--- src/components/views/HomeView.tsx
+++ src/components/views/HomeView.tsx
@@ -3,15 +3,10 @@ import styles from "./HomeView.module.scss";
 import ApiCard from "./ApiCard/ApiCard";
 
 const HomeView: React.FC = () => {
-    const [count, setCount] = React.useState(0);
-
     return (
         <div className={styles.home_view_container}>
             <div className={styles.content_section}>
-                <button onClick={() => { setCount(count + 1) }}>Increase count</button>
-                <ApiCard>
-                    Api Card
-                </ApiCard>
+                <ApiCard />
             </div>
         </div>
     )
